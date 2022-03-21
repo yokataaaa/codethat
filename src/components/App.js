1b8889363d0191b9dace59eb20b1c58eea65d3f3
LAPTOP-7CH5KCTU\dlawp <dlawpgur84@gmail.com>
2022-03-21 16:13:43 +0900

    2022-03-21

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import Nav from "../components/Nav";
+import Footer from "../components/Footer";
+import styles from "./App.module.css";
+import "./App.font.css";
+import { Outlet } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Nav className={styles.nav} />
+      <div className={styles.body}>
+        <Outlet />
+      </div>
+      <Footer className={styles.footer} />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/CoursePage.js
@@ -0,0 +1,53 @@
+import { addWishlist, getCourseBySlug } from "../api";
+import Button from "../components/Button";
+import Container from "../components/Container";
+import Card from "../components/Card";
+import CourseIcon from "../components/CourseIcon";
+import getCourseColor from "../utils/getCourseColor";
+import styles from "./CoursePage.module.css";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
+
+function CoursePage() {
+  const { courseSlug } = useParams();
+  const navigate = useNavigate();
+  const course = getCourseBySlug(courseSlug);
+  const courseColor = getCourseColor(course?.code);
+
+  if (!course) {
+    return <Navigate to="/courses" />;
+  }
+
+  const headerStyle = {
+    borderTopColor: courseColor,
+  };
+
+  const handleAddWishlistClick = () => {
+    addWishlist(course?.slug);
+    navigate("/wishlist");
+  };
+
+  return (
+    <>
+      <div className={styles.header} style={headerStyle}>
+        <Container className={styles.content}>
+          <CourseIcon photoUrl={course.photoUrl} />
+          <h1 className={styles.title}>{course.title}</h1>
+          <Button variant="round" onClick={handleAddWishlistClick}>
+            + 코스 담기
+          </Button>
+          <p className={styles.summary}>{course.summary}</p>
+        </Container>
+      </div>
+      <Container className={styles.topics}>
+        {course.topics.map(({ topic }) => (
+          <Card className={styles.topic} key={topic.slug}>
+            <h3 className={styles.title}>{topic.title}</h3>
+            <p className={styles.summary}>{topic.summary}</p>
+          </Card>
+        ))}
+      </Container>
+    </>
+  );
+}
+
+export default CoursePage;
